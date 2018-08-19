5a17e7bf659f6eec6897204242aecaea768648b6
wincy <wincychan2003@yahoo.com.hk>
2018-08-19 21:01:11 +0800

    Add Users component module

---
+++ src/Screens/Users.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import { Platform, StyleSheet } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Text, Body, Button, Left, Right, Icon } from "native-base";
+import axios from 'axios';
+import _ from 'lodash';
+
+export default class Users extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          users: []
+        };
+    
+      }
+    
+      componentDidMount() {
+        this.getUsers();
+      }
+    
+      
+      getUsers = () => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+         .then((response) => {
+             console.log(response)
+          this.setState({users: response.data});
+         })
+        .catch((error)=>{
+           console.log(error);
+        });
+      }
+    
+      renderButtons() {
+        return this.state.users.map((item) => {
+            return (
+            <Card key={item.id}>
+            <CardItem>
+              <Left>
+                <Icon name="ios-person"/>
+                <Body>
+                  <Text>{item.name}</Text>
+                  <Text note>{item.phone}</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent onPress={() => this.props.navigation.navigate('UserDetails', {id: item.id})} block>
+                  <Icon active name="ios-contact" />
+                  <Text>View</Text>
+                </Button>
+              </Left>
+              <Right>
+                <Button transparent onPress={() => this.props.navigation.navigate('Todos', {id: item.id})} block>
+                <Icon active name="ios-list" />
+                  <Text>To-do</Text>
+                </Button>
+              </Right>
+            </CardItem>
+            </Card>
+
+
+
+
+
+
+
+            );
+        });
+    }
+    
+      render() {
+        return (
+          <Container>
+            <Content padder>
+                {this.renderButtons()}
+            </Content>
+          </Container>
+        );
+      }
+    
+}
